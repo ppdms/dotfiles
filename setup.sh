6ad7f29794a230d95255b309e4da076fd26debb2
Basil Papadimas <basil@excession.gr>
2023-03-03 13:18:02 +0200

    bash script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# symlink: $ ln -s origin destination
+
+ln -s /Users/basil/Developer/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s /Users/basil/Developer/dotfiles/zshrc ~/.zshrc
+ln -s /Users/basil/Developer/dotfiles/vimrc ~/.vimrc
---
+++ vimrc
@@ -0,0 +1,13 @@
+call plug#begin()
+Plug 'ActivityWatch/aw-watcher-vim'
+call plug#end()
+set nocompatible
+filetype plugin indent on
+syntax on
+set expandtab
+set ai
+set number
+set hlsearch
+set ruler
+set backspace=indent,eol,start
+highlight Comment ctermfg=green
