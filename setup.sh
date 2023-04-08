70d6497a29022c0ae5e44e33508c6429aee63502
Basil Papadimas <basil@excession.gr>
2023-04-08 14:32:07 +0300

    😬

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # symlink: $ ln -s origin destination
 
-ln -s /Users/basil/Developer/dotfiles/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -s /Users/basil/Developer/dotfiles/zshrc ~/.zshrc
-ln -s /Users/basil/Developer/dotfiles/vimrc ~/.vimrc
+ln -s /Users/basil/Documents/Developer/dotfiles/settings.json /Users/basil/Library/Application\ Support/Code/User/settings.json
+ln -s /Users/basil/Documents/Developer/dotfiles/zshrc /Users/basil/.zshrc
+ln -s /Users/basil/Documents/Developer/dotfiles/vimrc /Users/basil/.vimrc
